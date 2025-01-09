a6b89d2fff43b39a4aba7f1aef6b6cba62fd00fd
Omar Alaa <oak1512001@gmail.com>
2025-01-09 21:51:45 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,232 @@
+function Category(element) {
+  const category = element.closest(".filter-category");
+  const items = category.querySelector(".filter-items");
+  const arrow = category.querySelector(".arrow");
+
+  if (items.style.display === "block") {
+    items.style.display = "none";
+    arrow.innerHTML = `<img src="../photos/Path22.png" alt="" srcset="">`; // Arrow down
+  } else {
+    items.style.display = "block";
+    arrow.innerHTML = `<img src="../photos/Path13.png" alt="" srcset="">`; // Arrow up
+  }
+}
+
+let men = document.getElementById("men");
+console.log(men);
+men.addEventListener("change", () => {
+  console.log(men.checked);
+});
+
+var gridone = document.getElementById("gridone");
+gridone.addEventListener("click", () => {
+  document.querySelector(".container").style = `display: grid;
+  grid-template-columns: repeat(1, 1fr);`;
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.style = `display:flex;max-width:100%`;
+  });
+  const imageContainer = document.querySelectorAll(".img-contanier");
+  imageContainer.forEach((imageContainer) => {
+    imageContainer.style = "width:35%";
+  });
+  const data = document.querySelectorAll(".data");
+  data.forEach((data) => {
+    data.style = `justify-content:start; align-items:start; margin-left:20px`;
+  });
+  const itemName = document.querySelectorAll(".item-name");
+  itemName.forEach((name) => {
+    name.style.fontSize = "18px";
+  });
+});
+
+var gridtwo = document.getElementById("gridtwo");
+gridtwo.addEventListener("click", () => {
+  document.querySelector(".container").style = `display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: space-between;`;
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.style.maxWidth = "600px";
+    card.style.display = "block";
+  });
+
+  const imageContainer = document.querySelectorAll(".img-contanier");
+  imageContainer.forEach((imageContainer) => {
+    imageContainer.style = "width:100%";
+  });
+  const data = document.querySelectorAll(".data");
+  data.forEach((data) => {
+    data.style = `justify-content:center; align-items:center; margin-left:0px`;
+  });
+
+  const itemName = document.querySelectorAll(".item-name");
+  itemName.forEach((name) => {
+    name.style.fontSize = "18px";
+  });
+
+  const images = document.querySelectorAll(".main-image");
+  images.forEach((image) => {
+    image.style.width = "100%";
+  });
+});
+
+var gridthree = document.getElementById("gridthree");
+gridthree.addEventListener("click", () => {
+  document.querySelector(".container").style = `display: grid;
+  grid-template-columns: repeat(3, 1fr);`;
+  const itemName = document.querySelectorAll(".item-name");
+  itemName.forEach((name) => {
+    name.style.fontSize = "14px";
+  });
+
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.style.maxWidth = "400px";
+    card.style.display = "block";
+  });
+
+  const imageContainer = document.querySelectorAll(".img-contanier");
+  imageContainer.forEach((imageContainer) => {
+    imageContainer.style = "width:100%";
+  });
+  const data = document.querySelectorAll(".data");
+  data.forEach((data) => {
+    data.style = `justify-content:center; align-items:center; margin-left:0px`;
+  });
+});
+var gridFour = document.getElementById("gridFour");
+gridFour.addEventListener("click", () => {
+  document.querySelector(".container").style = `display: grid;
+  grid-template-columns: repeat(4, 1fr);`;
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.style.maxWidth = "300px";
+    card.style.display = "block";
+  });
+
+  const imageContainer = document.querySelectorAll(".img-contanier");
+  imageContainer.forEach((imageContainer) => {
+    imageContainer.style = "width:100%";
+  });
+  const data = document.querySelectorAll(".data");
+  data.forEach((data) => {
+    data.style = `justify-content:center; align-items:center; margin-left:0px`;
+  });
+  const itemName = document.querySelectorAll(".item-name");
+  itemName.forEach((name) => {
+    name.style.fontSize = "11px";
+  });
+});
+
+//search function
+// document.getElementById("Search").addEventListener("click", () => {
+//     let searchInput = document.getElementById("searchInput").value;
+//     let filteredProducts = jsonData.collection.productVariants.filter((product) =>
+//       product.untranslatedTitle.toLowerCase().includes(searchInput.toLowerCase())
+//     );
+//     console.log(filteredProducts);
+// });
+
+///////Nav Bar///////
+var logo = document.getElementById("logo");
+logo.onmouseover = function () {
+  logo.style.cursor = "pointer";
+};
+//////////////linksHover//////////////////
+var links = document.getElementsByClassName("link");
+function linkOverFun(event) {
+  event.target.style.textDecoration = "underline";
+  event.target.style.cursor = "pointer";
+}
+
+function linkOutFun(event) {
+  event.target.style.textDecoration = "none";
+}
+for (link of links) {
+  link.onmouseover = linkOverFun;
+  link.onmouseout = linkOutFun;
+}
+///////////////iconsHover////////////////
+var icons = document.getElementsByClassName("icon");
+function iconOverFun(event) {
+  event.target.style.transform = "scale(1.3)";
+  event.target.style.transitionDuration = "0.5s";
+  event.target.style.transitionTimingFunction = "ease-out";
+  event.target.style.cursor = "pointer";
+}
+function iconOutFun(event) {
+  event.target.style.transform = "scale(1)";
+  event.target.style.transitionDuration = "0.5s";
+  event.target.style.transitionTimingFunction = "ease-in";
+}
+for (icon of icons) {
+  icon.onmouseover = iconOverFun;
+  icon.onmouseout = iconOutFun;
+}
+
+/////////FOOTER/////////
+var links = document.getElementsByClassName("link");
+function linkOverFun(event) {
+  event.target.style.textDecoration = "underline";
+  event.target.style.cursor = "pointer";
+}
+
+function linkOutFun(event) {
+  event.target.style.textDecoration = "none";
+}
+for (link of links) {
+  link.onmouseover = linkOverFun;
+  link.onmouseout = linkOutFun;
+}
+
+function viewProduct() {
+  let viewProduct = document.querySelectorAll(".viewproduct");
+  viewProduct.forEach((element) => {
+    element.addEventListener("click", function () {
+      let productId = element.getAttribute("data-id");
+      window.location.assign("./Product/product.html?id=" + productId);
+    });
+  });
+}
+
+function logout() {
+  localStorage.removeItem("currentUser");
+  window.location.href = "./index.html";
+}
+
+// let data;
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', './json/winter.json');
+// xhr.onreadystatechange = function() {
+//   if(xhr.readyState === 4 && xhr.status === 200) {
+//     data = JSON.parse(xhr.responseText);
+//     console.log(data);
+//     console.log(data.collection.winter.length);
+//   }
+// };
+// xhr.send();
+
+// let data;
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', './json/summer.json');
+// xhr.onreadystatechange = function() {
+//   if(xhr.readyState === 4 && xhr.status === 200) {
+//     data = JSON.parse(xhr.responseText);
+//     console.log(data);
+//     console.log(data.collection.summer.length);
+//   }
+// };
+// xhr.send();
+
+// let data;
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', './json/Kids.json');
+// xhr.onreadystatechange = function() {
+//   if(xhr.readyState === 4 && xhr.status === 200) {
+//     data = JSON.parse(xhr.responseText);
+//     console.log(data);
+//     console.log(data.collection.kids.length);
+//   }
+// };
+// xhr.send();
---
+++ js/winter.js
@@ -0,0 +1,108 @@
+let data;
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "../json/winter.json");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    data = JSON.parse(xhr.responseText);
+    displayData(data.collection.winter);
+  }
+};
+xhr.send();
+
+function displayData(data) {
+  let allContainer = document.querySelector(".container");
+
+  data.forEach((item) => {
+    let card = document.createElement("div");
+    card.className = "card";
+
+    const saleDiv = document.createElement("div");
+    saleDiv.className = "sale";
+
+    const sale = document.createElement("div");
+    sale.className = "sale-text";
+    sale.textContent = "Sale 60%";
+
+    const newDiv = document.createElement("div");
+    newDiv.className = "new-text";
+    newDiv.textContent = "NEW";
+
+    saleDiv.appendChild(sale);
+    saleDiv.appendChild(newDiv);
+
+    const imgContainer = document.createElement("div");
+    imgContainer.className = "img-contanier";
+    imgContainer.appendChild(saleDiv);
+
+    const mainImage = document.createElement("img");
+    mainImage.src = item.image.src;
+    mainImage.alt = item.product.title;
+    mainImage.className = "main-image";
+    mainImage.setAttribute("id", item.product.id);
+
+    const buttonDiv = document.createElement("div");
+    const button = document.createElement("button");
+    button.type = "button";
+    button.textContent = "QUICK ADD";
+
+    buttonDiv.appendChild(button);
+    imgContainer.appendChild(mainImage);
+    imgContainer.appendChild(buttonDiv);
+    card.appendChild(imgContainer);
+
+    const dataDiv = document.createElement("div");
+    dataDiv.className = "data";
+
+    const dataInfo = document.createElement("div");
+    dataInfo.className = "dataInfo";
+
+    const itemName = document.createElement("div");
+    itemName.className = "item-name";
+
+    const productName = document.createElement("p");
+    productName.textContent = item.product.title;
+
+    const productType = document.createElement("p");
+    productType.textContent = item.product.type;
+
+    itemName.appendChild(productName);
+    itemName.appendChild(document.createTextNode(" - "));
+    itemName.appendChild(productType);
+    dataInfo.appendChild(itemName);
+    dataDiv.appendChild(dataInfo);
+
+    const priceDiv = document.createElement("div");
+    priceDiv.className = "price";
+
+    const priceText = document.createElement("p");
+    priceText.textContent = `${item.price.currencyCode} ${item.price.amount}`;
+    priceDiv.appendChild(priceText);
+
+    dataDiv.appendChild(priceDiv);
+
+    let smallImg = document.createElement("img");
+    smallImg.src = item.image.src;
+    smallImg.width = 50;
+    smallImg.height = 50;
+
+    dataDiv.appendChild(smallImg);
+    card.appendChild(dataDiv);
+
+    allContainer.appendChild(card);
+    viewProduct();
+  });
+}
+let cartButton = document.createElement("button");
+cartButton.textContent = "View Cart";
+cartButton.addEventListener("click", opencart);
+document.body.appendChild(cartButton);
+
+function viewProduct() {
+  const cardId = document.querySelectorAll(".main-image");
+  cardId.forEach((card) => {
+    card.addEventListener("click", () => {
+      const productId = card.getAttribute("id");
+      window.location.assign(`./product.html?id=${productId}`);
+    });
+  });
+}
